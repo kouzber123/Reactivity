9f5bb9dd792cb7222679b1c997eb21a9d44a8688
kouzber123 <tom.nguyen@windowslive.com>
2023-04-14 15:30:51 +0300

    feature added bio text in the joined user card and added substring

--- client-app/src/features/profiles/ProfileCard.tsx
+++ client-app/src/features/profiles/ProfileCard.tsx
@@ -14,7 +14,7 @@ export default observer(function ProfileCard({ profile }: Props) {
       <Image src={profile.image || "/assets/user.png"} />
       <Card.Content>
         <Card.Header>{profile.displayName}</Card.Header>
-        <Card.Description>Bio goes here</Card.Description>
+        <Card.Description>{profile.bio?.substring(0, 30)}...</Card.Description>
       </Card.Content>
       <Card.Content extra>
         <Icon name="user" />
